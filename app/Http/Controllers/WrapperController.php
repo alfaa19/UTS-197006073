af0b9d869a2952031bfd61279e23d13da7f86e8a
Alfa Adriel Monico <alfaa19@gmail.com>
2022-04-03 13:33:43 +0700

    Add Query API key params

--- app/Http/Controllers/WrapperController.php
+++ app/Http/Controllers/WrapperController.php
@@ -7,32 +7,36 @@ use Illuminate\Support\Facades\Http;
 
 class WrapperController extends Controller
 {
-    public function sharddata()
+    public function sharddata(Request $request)
     {
-        $json = Http::get( 'https://na1.api.riotgames.com/lol/status/v3/shard-data?api_key=RGAPI-172fdc39-fb5d-47f9-b9b4-f49668b2eecd')->json();
+        $key = $request->api_key;
+        $json = Http::get( 'https://na1.api.riotgames.com/lol/status/v3/shard-data?api_key='.$key)->json();
         return response()->json($json);
     }
 
-    public function championRotations(){
-        $json = Http::get( 'https://na1.api.riotgames.com/lol/platform/v3/champion-rotations?api_key=RGAPI-172fdc39-fb5d-47f9-b9b4-f49668b2eecd')->json();
+    public function championRotations(Request $request){
+        $key = $request->api_key;
+        $json = Http::get( 'https://na1.api.riotgames.com/lol/platform/v3/champion-rotations?api_key='.$key)->json();
         return response()->json($json);
     }
 
-    public function summonerByName($summonerName){
-        $json = Http::get( 'https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/'.$summonerName.'?api_key=RGAPI-172fdc39-fb5d-47f9-b9b4-f49668b2eecd')->json();
+    public function summonerByName($summonerName, Request $request){
+        $key = $request->api_key;
+        $json = Http::get( 'https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/'.$summonerName.'?api_key='.$key)->json();
         return response()->json($json);
 
     }
 
     public function  allLeagueEntries($queue,$tier,$division, Request $request, $pge){
-        $page = $request->api_key;
+        $key = $request->api_key;
         $pges = $pge->page;
-        $json = Http::get('https://na1.api.riotgames.com/lol/league/v4/entries/'.$queue.'/'.$tier.'/'.$division.'?'.$pges.'&api_key='.$page)->json();
+        $json = Http::get('https://na1.api.riotgames.com/lol/league/v4/entries/'.$queue.'/'.$tier.'/'.$division.'?'.$pges.'&api_key='.$key)->json();
         return response()->json($json);
     }
 
-    public function featured(){
-        $json = Http::get('https://na1.api.riotgames.com/lol/spectator/v4/featured-games?api_key=RGAPI-172fdc39-fb5d-47f9-b9b4-f49668b2eecd')->json();
+    public function featured(Request $request){
+        $key = $request->api_key;
+        $json = Http::get('https://na1.api.riotgames.com/lol/spectator/v4/featured-games?api_key='.$key)->json();
         return response()->json($json);
     }
 
